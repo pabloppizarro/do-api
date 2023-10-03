797e11b3ef55c95f6f7260bbd9aadb562a518bed
pabloppizarro <pizarro.pablo@outlook.com>
2023-10-03 14:47:40 -0300

    added websocket server and client

---
+++ index-ws.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const server = require("http").createServer();
+
+const app = express();
+
+app.get("/", function (req, res) {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+server.on("request", app);
+
+server.listen(3000, function () {
+  console.log("Server started at 3000");
+});
+
+//WEB SOCKETS!
+
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({ server: server });
+
+//events
+wss.on("connection", (ws) => {
+  const numClients = wss.clients.size;
+  console.log("clients connected to sv websocket", numClients);
+  wss.broadcast(`New user connected, total: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to my server");
+  }
+
+  ws.on("close", () => {
+    console.log("A client has disconnected");
+    wss.broadcast(`user disconneted`);
+  });
+});
+
+wss.broadcast = (data) => {
+  wss.clients.forEach((c) => c.send(data));
+};
