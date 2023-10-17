0b90095a5afecdb69e51d2a6782b7c816d5b2024
pabloppizarro <pizarro.pablo@outlook.com>
2023-10-17 11:33:46 -0300

    added final catch to dolaSi get service

--- services/dolarSi.js
+++ services/dolarSi.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { HttpStatusCode } from "axios";
 import * as xm2 from "xml-js";
 
 export default async function getDolarSiData() {
@@ -6,5 +6,10 @@ export default async function getDolarSiData() {
     .get(process.env.DOLARSI_API)
     .then((res) => xm2.xml2json(res.data, { compact: true, spaces: 4 }))
     .then((json) => JSON.parse(json))
-    .catch((e) => e);
+    .catch((e) => {
+      throw {
+        error: e,
+        code: HttpStatusCode.InternalServerError,
+      };
+    });
 }
