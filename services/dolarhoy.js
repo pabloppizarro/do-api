ef53218c0e28bdf67fd6f5917ab7e9b8c814b759
pabloppizarro <pizarro.pablo@outlook.com>
2023-10-17 16:17:58 -0300

    added oficial usd from dolar-hoy

--- services/dolarhoy.js
+++ services/dolarhoy.js
@@ -1,14 +1,14 @@
 import * as jsdom from "jsdom";
 const { JSDOM } = jsdom;
 
-export default function getDolarHoyBlue() {
+export default function getDolarHoyBlue(url) {
   const options = {
-    referrer: process.env.DOLAR_HOY,
+    referrer: url,
     includeNodeLocations: true,
     storageQuota: 10000000,
   };
 
-  return JSDOM.fromURL(process.env.DOLAR_HOY, options).then((dom) => {
+  return JSDOM.fromURL(url, options).then((dom) => {
     // console.log(dom.serialize());
 
     const document = dom.window.document;
@@ -17,7 +17,6 @@ export default function getDolarHoyBlue() {
     const percentaje = null;
     let date = document.querySelector(".update").textContent;
     date = date.split(" ").slice(2).join(" ");
-    console.log("try", buy, sale);
     return { buy, sale, percentaje, date };
   });
 }
